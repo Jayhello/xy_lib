d9c218f090e2c52b2ad306b9cdc4f2253e2a742a
Jayhello <haluoluo@qq.com>
2023-09-06 08:22:28 +0800

    add new

--- src/comm/xy_thread.h
+++ src/comm/xy_thread.h
@@ -1,13 +1,28 @@
-//
-// Created by wenwen on 2023/3/12.
-//
+
 #include "noncopyable.h"
 #include "xy_exception.h"
 #include <thread>
 
 namespace xy{
 
+class ThreadException : public Exception{
+public:
+    ThreadException(const string &buffer) : Exception(buffer) {};
+    ~ThreadException(){};
+};
 
+class ThreadControl{
+public:
+    explicit ThreadControl(std::thread* th):_th(th){}
+
+    // 等待当前线程结束, 不能在当前线程上调用
+    void join();
+
+    // detach, 不能在当前线程上调用
+    void detach();
+
+    std::thread* _th;
+};
 
 class Thread : public noncopyable{
 public:
